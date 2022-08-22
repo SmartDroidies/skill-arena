e2e5fd4762f8c59ab8b7bf0850ebed124039600a
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-08-21 23:26:46 -0400

    Husky with ESlint

--- src/features/profile/screens/Profile.js
+++ src/features/profile/screens/Profile.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 const Profile = () => {
   const userName = "Prasath B";
@@ -10,21 +10,15 @@ const Profile = () => {
       <Text>Name : {userName}</Text>
       <Text>E-Mail : {userEMail}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 50,
     marginLeft: 20,
-    backgroundColor: '#567356'
   },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44
-  }
-})
+});
 
-export default Profile
+export default Profile;
