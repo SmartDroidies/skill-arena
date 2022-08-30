1a73aec710997ff5d8d3e223fb53e12a4c466590
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-08-29 23:10:51 -0400

    Section parameterized

--- src/features/home/components/Section.js
+++ src/features/home/components/Section.js
@@ -3,44 +3,11 @@ import { View, StyleSheet, FlatList } from "react-native";
 import { Text } from "@rneui/themed";
 import Course from "../../../components/Course";
 
-const courses = [
-  {
-    id: 1,
-    title: "Course 1",
-    author: "First Author",
-    price: "Rs 500",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 2,
-    title: "Course 2",
-    author: "Second Author",
-    price: "Rs 600",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 3,
-    title: "Course 3",
-    author: "Third Author",
-    price: "Rs 750",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 4,
-    title: "Course 4",
-    author: "Final Author",
-    price: "Rs 1000",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-];
-
-const Section = () => {
+const Section = ({ head, courses }) => {
   const renderCourseCard = ({ item }) => <Course course={item} />;
-  const sectionHead = "Section head";
-
   return (
     <View style={styles.container}>
-      <Text h4>{sectionHead}</Text>
+      <Text h4>{head}</Text>
       <FlatList
         horizontal={true}
         data={courses}
@@ -54,7 +21,7 @@ const Section = () => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20
+    marginTop: 20,
   },
 });
 
--- src/features/home/screens/Home.js
+++ src/features/home/screens/Home.js
@@ -3,6 +3,38 @@ import { StyleSheet, SafeAreaView, ScrollView, StatusBar } from "react-native";
 import { Divider } from "@rneui/themed";
 import Section from "../components/Section";
 
+const courses = [
+  {
+    id: 1,
+    title: "Course 1",
+    author: "First Author",
+    price: "Rs 500",
+    image: "https://reactnative.dev/img/tiny_logo.png",
+  },
+  {
+    id: 2,
+    title: "Course 2",
+    author: "Second Author",
+    price: "Rs 600",
+    image: "https://reactnative.dev/img/tiny_logo.png",
+  },
+  {
+    id: 3,
+    title: "Course 3",
+    author: "Third Author",
+    price: "Rs 750",
+    image: "https://reactnative.dev/img/tiny_logo.png",
+  },
+  {
+    id: 4,
+    title: "Course 4",
+    author: "Final Author",
+    price: "Rs 1000",
+    image: "https://reactnative.dev/img/tiny_logo.png",
+  },
+];
+
+
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
@@ -10,13 +42,13 @@ const Home = () => {
         style={styles.scrollView}
         showsVerticalScrollIndicator={false}
       >
-        <Section></Section>
+        <Section head="First Section" courses={courses}></Section>
         <Divider />
 
-        <Section></Section>
+        <Section head="Second Section" courses={courses}></Section>
         <Divider />
 
-        <Section></Section>
+        <Section head="Third Section" courses={courses}></Section>
         <Divider />
       </ScrollView>
     </SafeAreaView>
