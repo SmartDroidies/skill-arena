14d1c796511a97f914396841f0d2be1f96c9ac0d
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-09-16 00:04:55 -0400

    Course displayed in list

---
+++ src/components/Skeleton.js
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from "react-native";
+
+const Skeleton = () => <View style={styles.sample}>
+    
+</View>;
+
+const styles = StyleSheet.create({});
+
+export default Skeleton;
--- src/features/course/screens/Course.js
+++ src/features/course/screens/Course.js
@@ -1,11 +1,48 @@
 import { React } from "react";
-import { Card } from "@rneui/themed";
+import { StyleSheet, SafeAreaView, ScrollView, StatusBar,FlatList } from "react-native";
+import Category from "../components/Category";
+
+const categories = [
+  {
+    title: "Art",
+  },
+  {
+    title: "Music",
+  },
+  {
+    title: "SoftSkill",
+  },
+  {
+    title: "Fitness",
+  },
+  {
+    title: "Lifestyle",
+  },
+];
+
+const renderCategory = ({ item }) => <Category category={item} />;
 
 const Course = () => (
-  <Card>
-    <Card.Title></Card.Title>
-    <Card.Divider />
-  </Card>
+  <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+      <FlatList
+        data={categories}
+        renderItem={renderCategory}
+        keyExtractor={(item) => item.title}
+        showsHorizontalScrollIndicator={false}
+      />
+    </ScrollView>
+  </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  scrollView: {
+    marginHorizontal: 20,
+    marginVertical: 20,
+  },
+});
 
 export default Course;
