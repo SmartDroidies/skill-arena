8f2cd91619cd57dcb9fcfe1cc964f2f13a1ec65b
Arjun <arjun.251106@gmail.com>
2022-09-16 09:05:27 +0530

    coursescreen

--- src/features/course/screens/Course.js
+++ src/features/course/screens/Course.js
@@ -1,10 +1,18 @@
-import { React }  from "react";
-import { Text } from "react-native";
+import { React } from "react";
+import { Card } from "@rneui/themed";
 
-const Course = () => {
-  return (
-    <Text>Hello Visit my Page!</Text>
-  );
-};
+const Course = () => (
+
+  <Card>
+
+    <Card.Title></Card.Title>
+
+    <Card.Divider />
+
+  </Card>
+
+);
+
+
 
 export default Course;
