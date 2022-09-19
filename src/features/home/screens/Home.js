c95337558f172c107968c02540c9b23bfeb47fcb
karthick419 <karthickmohan419@gmail.com>
2022-09-19 08:07:56 +0530

    home

--- src/features/home/screens/Home.js
+++ src/features/home/screens/Home.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { Text } from "react-native";
+import axios from 'axios';
+const baseUrl = 'https://trello.com/c/6abUSJ35/51-home-screen-display-sections-and-courses-from-the-backend-service';
 
-const Home = () => {
-  return <Text>Home screen</Text>;
-};
+// Passing configuration object to axios
+axios({
+  method: 'get',
+  url: `${baseUrl}/api/users/1`,
+}).then((response) => {
+  console.log(response.data);
+});
 
-export default Home;
+// Invoking get method to perform a GET request
+axios.get(`${baseUrl}/api/users/1`).then((response) => {
+  console.log(response.data);
+});
