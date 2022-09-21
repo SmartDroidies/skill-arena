6c928857bab892f0f64808b700c9fb4a5b26456d
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-09-20 23:24:37 -0400

    Home Section displayed dynamically

--- src/features/home/screens/Home.js
+++ src/features/home/screens/Home.js
@@ -1,110 +1,33 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, ScrollView, StatusBar } from "react-native";
-import Section from "../components/Section";
-import courseClient from "../../../api/courseCient";
+import courseClient from "../../../api/courseClient";
 import { useEffect, useState } from "react";
-import { Divider } from "@rneui/themed";
+import CourseSection from "../components/CourseSection";
 
-// FIXME - Hard coded data to be removed
-const courses_books = [
-  {
-    id: 1,
-    title: "Course 1",
-    author: "First Author",
-    price: "Rs 500",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 2,
-    title: "Course 2",
-    author: "Second Author",
-    price: "Rs 600",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 3,
-    title: "Course 3",
-    author: "Third Author",
-    price: "Rs 750",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 4,
-    title: "Course 4",
-    author: "Final Author",
-    price: "Rs 1000",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-];
+const Home = () => {
+  const [course_section, setSection] = useState([]);
 
-// FIXME - Hard coded data to be removed
-const courses_health = [
-  {
-    id: 1,
-    title: "Health Course 1",
-    author: "First Author",
-    price: "Rs 500",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 2,
-    title: "Health Course 2",
-    author: "Second Author",
-    price: "Rs 600",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 3,
-    title: "Health Course 3",
-    author: "Third Author",
-    price: "Rs 750",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-  {
-    id: 4,
-    title: "Health Course 4",
-    author: "Final Author",
-    price: "Rs 1000",
-    image: "https://reactnative.dev/img/tiny_logo.png",
-  },
-];
-
-//FIXME - Move this to an hook
-const hook = () => {
-  const [course, setCourse] = useState([]);
-  const getCourse = () => {
+  useEffect(() => {
     courseClient
       .get("/home")
       .then((response) => {
-        setItem(response.data);
-        console.log(response.data);
+        setSection(response.data);
+        // console.log(response.data);
       })
       .catch((error) => {
         console.log("Error :", error);
       });
-    }
-    useEffect(() => {
-      getCourse();
-    }, course);
-    
-}
+  });
 
-const Home = (course) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView
         style={styles.scrollView}
         showsVerticalScrollIndicator={false}
       >
-        {/* <Section head="First Section" courses={courses_books}></Section> */}
-        <Section head={course.name}></Section>
-        <Divider />
-
-        <Section head="Second Section" courses={courses_health}></Section>
-        <Divider />
-
-        <Section head="Third Section" courses={courses_health}></Section>
-        <Divider />
+        {course_section.map((courseSec) => (
+          <CourseSection content={courseSec}></CourseSection>
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
