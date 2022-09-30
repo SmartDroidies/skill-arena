cc5639b5ab86bbe16d60b7625320ce7f2eee610e
Arjun <arjun.251106@gmail.com>
2022-09-30 08:02:48 +0530

    listcategory

--- src/features/course/screens/ListCategory.js
+++ src/features/course/screens/ListCategory.js
@@ -1,4 +1,12 @@
-import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  FlatList,
+  numColumns,
+  Text,
+} from "react-native";
 import { useEffect, useState } from "react";
 import courseClient from "../../../api/courseClient";
 import Category from "../components/Category";
@@ -6,11 +14,12 @@ import Category from "../components/Category";
 const ListCategory = () => {
   const [category, setCategory] = useState([]);
 
+
   useEffect(() => {
     courseClient
       .get("/category")
       .then((response) => {
-        console.log(response.data);
+        //console.log(response.data);
         setCategory(response.data);
       })
       .catch((error) => {
@@ -18,19 +27,70 @@ const ListCategory = () => {
       });
   });
 
+  const item = () => {
+    { id: 'work', value; 'WORk' }
+    { id: 'hobbies', value; 'HOBBIES' }
+    { id: 'fitness', value; 'FITNESS' }
+    { id: 'acedamy', value; 'ACEDAMY' }
+    { id: 'sports', value; 'SPORTS' }
+    { id: 'arts', value; 'ARTS' }
+    { id: 'music', value; 'MUSIC' }
+    { id: 'lifestyle', value; 'LIFESTYLE' }
+  };
+
+
   return (
+
     <SafeAreaView style={styles.container}>
+
       <ScrollView
+
         style={styles.scrollView}
         showsVerticalScrollIndicator={false}
       >
+        <View style={styles.itemContainer}>
+          <Text>{item} </Text>
+        </View>
+        <FlatList
+          horizontal={true}
+          data={item}
+          renderItem={item}
+          keyExtractor={(item) => item.id}
+          numColumns={numColumns}
+          showsHorizontalScrollIndicator={false}
+        />
+
+
         {category.map((category) => (
           <Category content={category}></Category>
         ))}
       </ScrollView>
-    </SafeAreaView>
+    </SafeAreaView >
   );
 };
-const styles = StyleSheet.create({});
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: "#7cb48f",
+  },
+
+  scrollView: {
+    marginHorizontal: "auto",
+    marginVertical: 10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: "#7cb48f",
+    width: 400,
+    flex: 8,
+
+  }
+
+
+});
+
 
 export default ListCategory;
