e4437b3c77aec58523ca5a110364f59f5426cf98
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-10-10 23:13:42 -0400

    fixing errors

--- src/features/course/components/Category.js
+++ src/features/course/components/Category.js
@@ -1,34 +1,24 @@
 import { StyleSheet, View } from "react-native";
 import { Card, Text, Button } from "@rneui/themed";
-import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 
+const Category = ({ content, navigation }) => (
+  <View style={styles.sample}>
+    <Card>
+      <Card.Title>
+        <Text h4>{content.code}</Text>
+        <Button
+          titleStyle={styles.link}
+          title="Click"
+          onPress={() =>
+            navigation.navigate("ListCourse", { code: content.code })
+          }
+          type="clear"
+        />
+      </Card.Title>
+    </Card>
+  </View>
+);
 
-  const Category = ({ content, navigation }) => (
-    <View style={styles.sample}>
-      <Card>
-        <Card.Title>
-          <Text h4>{content.code}</Text>
-          <Button
-            titleStyle={styles.link}
-            title="Click"
-            onPress={() =>
-              navigation.navigate("ListCourse", { code: content.code })
-            }
-            type="clear"
-          />
-          return (
-          <View style={{ flex: 1 }}>
-            <Button onPress={() => navigationRef.navigate('Course')}>
-              Go courses
-            </Button>
-            <NavigationContainer ref={navigationRef}>{/* ... */}</NavigationContainer>
-          </View>
-          );
-        </Card.Title>
-      </Card>
-    </View>
-  );
+const styles = StyleSheet.create({});
 
-  const styles = StyleSheet.create({});
-
-  export default Category;
+export default Category;
