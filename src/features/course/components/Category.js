50e966e90ab3ee12a7a165f225eefa0087a016d0
Arjun <arjun.251106@gmail.com>
2022-10-17 22:15:21 +0530

    adjust width height

--- src/features/course/components/Category.js
+++ src/features/course/components/Category.js
@@ -1,12 +1,16 @@
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
 import { Card, Text } from "@rneui/themed";
-
 const Category = ({ content, navigation }) => (
+
   <View>
     <TouchableOpacity
       onPress={() => navigation.navigate("ListCourse", { code: content.code })}
     >
       <Card>
+        <View style={{
+          width: 90, height: 90, flex: 1, backgroundColor: '#00a2ed'
+
+        }} />
         <Card.Title>
           <Text h4>{content.title}</Text>
         </Card.Title>
@@ -16,3 +20,15 @@ const Category = ({ content, navigation }) => (
 );
 
 export default Category;
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+
+});
