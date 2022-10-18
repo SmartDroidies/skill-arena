dd7faa06915c21a009fadcb36714f1fee3d61a46
lokesh r <lokeshrajeshbabu28@gmail.com>
2022-10-18 22:27:56 +0530

    Image

--- src/components/Course.js
+++ src/components/Course.js
@@ -5,18 +5,18 @@ const Course = ({ course, navigation }) => (
   <View style={styles.course_align}>
     <TouchableOpacity onPress={() => navigation.navigate("CourseDetail")}>
       <Card containerStyle={styles.course_card}>
-          <Image
-            style={styles.course_image}
-            resizeMode="cover"
-            source={{
-              uri: course.image,
-            }}
-          />
-          <View style={styles.course_text}>
-            <Text>{course.course_name}</Text>
-            <Text>{course.author}</Text>
-            <Text>{course.price}</Text>
-          </View>
+        <Image
+          style={styles.course_image}
+          resizeMode="cover"
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <View style={styles.course_text}>
+          <Text>{course.course_name}</Text>
+          <Text>{course.author}</Text>
+          <Text>{course.price}</Text>
+        </View>
       </Card>
 
     </TouchableOpacity>
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
   },
   course_image: {
     width: 100,
-    height: 50,
+    height: 100,
   },
   course_text: {
     fontSize: 100,
