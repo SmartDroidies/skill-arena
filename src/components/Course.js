b81a6ac2cfc69ffa65efaf89f74cdcf1b1656740
lokesh r <lokeshrajeshbabu28@gmail.com>
2022-10-19 22:59:51 +0530

    styled

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,37 +1,42 @@
-import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
 import { Text, Card, Image } from "@rneui/themed";
+import styled from "styled-components/native";
+
 
 const Course = ({ course, navigation }) => (
   <View style={styles.course_align}>
-    <TouchableOpacity onPress={() => navigation.navigate("CourseDetail")}>
-      <Card containerStyle={styles.course_card}>
-        <Image
-          style={styles.course_image}
-          resizeMode="cover"
-          source={{
-            uri: "https://reactnative.dev/img/tiny_logo.png",
-          }}
-        />
-        <View style={styles.course_text}>
-          <Text>{course.title}</Text>
-          <Text>{course.author}</Text>
-          <Text>{course.price}</Text>
-        </View>
-      </Card>
-    </TouchableOpacity>
-  </View>
+    <Container>
+      <TouchableOpacity onPress={() => navigation.navigate("CourseDetail")}>
+        <Card containerStyle={styled.course_card}>
+          <Image
+            style={styled.course_image}
+            resizeMode="stretch"
+            source={{
+              uri: "https://reactnative.dev/img/tiny_logo.png",
+            }}
+          />
+          <View style={styles.course_text}>
+            <Text>{course.title}</Text>
+            <Text>{course.author}</Text>
+            <Text>{course.price}</Text>
+          </View>
+        </Card>
+      </TouchableOpacity>
+    </Container>
+  </View >
 );
 
 // FIXME  Create a styled component
-const styles = StyleSheet.create({
-  // eslint-disable-next-line react-native/no-color-literals
+const Container = styled.View`
   course_card: {
-    borderColor: "blue",
-  },
+    borderColor: "blue";
+  },  
   course_image: {
     width: 100,
     height: 100,
   },
+  `;
+const styles = StyleSheet.create({
   course_text: {
     fontSize: 100,
     marginTop: 20,
@@ -40,5 +45,6 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
 });
+
 
 export default Course;
