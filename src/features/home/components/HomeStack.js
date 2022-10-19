8b93bf1052fde0fad46198c65647ec3fa962bb90
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-10-18 22:45:47 -0400

    Home screen header

--- src/features/home/components/HomeStack.js
+++ src/features/home/components/HomeStack.js
@@ -1,4 +1,5 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import Global from "../../../utils/Global";
 import CourseDetail from "../screens/CourseDetail";
 import Home from "../screens/Home";
 
@@ -6,9 +7,17 @@ export const HomeStack = createStackNavigator();
 
 export const HomeNavigator = () => {
   return (
-    <HomeStack.Navigator screenOption>
-      <HomeStack.Screen name="Home" component={Home} />
-      <HomeStack.Screen name="CourseDetail" component={CourseDetail} />
+    <HomeStack.Navigator screenOptions={{ headerShown: true }}>
+      <HomeStack.Screen
+        name="Home"
+        component={Home}
+        options={{ title: Global.Constant.appName }}
+      />
+      <HomeStack.Screen
+        name="CourseDetail"
+        component={CourseDetail}
+        options={{ title: Global.Constant.appName }}
+      />
     </HomeStack.Navigator>
   );
 };
---
+++ src/utils/Global.js
@@ -0,0 +1,5 @@
+export default {
+  Constant: {
+    appName: "Skill Playground",
+  },
+};
