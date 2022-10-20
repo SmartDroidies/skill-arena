74e7b8b13ffe2479138149a7ff403233dbc8bbf9
lokesh r <lokeshrajeshbabu28@gmail.com>
2022-10-20 22:06:59 +0530

    styling

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,42 +1,37 @@
 import { View, TouchableOpacity, StyleSheet } from "react-native";
 import { Text, Card, Image } from "@rneui/themed";
-import styled from "styled-components/native";
 
 
 const Course = ({ course, navigation }) => (
   <View style={styles.course_align}>
-    <Container>
-      <TouchableOpacity onPress={() => navigation.navigate("CourseDetail")}>
-        <Card containerStyle={styled.course_card}>
-          <Image
-            style={styled.course_image}
-            resizeMode="stretch"
-            source={{
-              uri: "https://reactnative.dev/img/tiny_logo.png",
-            }}
-          />
-          <View style={styles.course_text}>
-            <Text>{course.title}</Text>
-            <Text>{course.author}</Text>
-            <Text>{course.price}</Text>
-          </View>
-        </Card>
-      </TouchableOpacity>
-    </Container>
+    <TouchableOpacity onPress={() => navigation.navigate("CourseDetail")}>
+      <Card containerStyle={styles.course_card}>
+        <Image
+          style={styles.course_image}
+          resizeMode="stretch"
+          source={{
+            uri: "https://reactnative.dev/img/tiny_logo.png",
+          }}
+        />
+        <View style={styles.course_text}>
+          <Text>{course.title}</Text>
+          <Text>{course.author}</Text>
+          <Text>{course.price}</Text>
+        </View>
+      </Card>
+    </TouchableOpacity>
   </View >
 );
 
 // FIXME  Create a styled component
-const Container = styled.View`
+const styles = StyleSheet.create({
   course_card: {
-    borderColor: "blue";
-  },  
+    borderColor: "blue",
+  },
   course_image: {
     width: 100,
     height: 100,
   },
-  `;
-const styles = StyleSheet.create({
   course_text: {
     fontSize: 100,
     marginTop: 20,
