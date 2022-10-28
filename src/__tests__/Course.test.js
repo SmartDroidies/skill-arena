69462d3d804c92874eea4eb35d62aaa24b83a178
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-10-27 22:44:23 -0400

    Testing setup

---
+++ src/__tests__/Course.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import renderer from "react-test-renderer";
+describe();
+
+// it("renders correctly", () => {
+//   const tree = renderer.create(<Course />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
