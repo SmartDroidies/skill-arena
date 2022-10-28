4a272d12c605ebff51c8847f27b2df3fd0241e6f
lokesh <lokeshrajeshbabu28@gmail.com>
2022-10-28 07:38:45 +0530

    testing

--- src/components/Course-test.js
+++ src/components/Course-test.js
@@ -3,10 +3,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Course from "../components/Course";
 
-// test('renders correctly', () => {
-//     const tree = renderer.create(<Course />).toJSON();
-//     expect(tree).toMatchSnapshot();
-// });
 it('renders correctly', () => {
     const tree = renderer
         .create(<Course />)
