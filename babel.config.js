b3d61d3c9ab3ee91a949d0f4d9be29f93c3411b7
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-10-29 21:17:37 -0400

    Test framework configured

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-  };
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//   };
+// };
+
+module.exports = {
+  presets: ['@babel/preset-env'],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-runtime"]
+    }
+  }
 };
