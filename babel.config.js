d5409210d8c1b4fca25d341bfaab1af20cb6a0a3
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-10-29 23:07:44 -0400

    Babel config

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,3 @@ module.exports = function(api) {
     presets: ["babel-preset-expo"],
   };
 };
-
-// module.exports = {
-//   presets: ["@babel/preset-env", "@babel/preset-typescript"],
-//   env: {
-//     test: {
-//       plugins: ["@babel/plugin-transform-runtime"],
-//     },
-//   },
-// };
