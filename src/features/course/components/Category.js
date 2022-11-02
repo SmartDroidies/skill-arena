aaaf37b771cbdef22bfbbbbfaa354bd4fc0864d7
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-11-01 23:22:53 -0400

    Ordering

--- src/features/course/components/Category.js
+++ src/features/course/components/Category.js
@@ -2,22 +2,21 @@ import { View, TouchableOpacity } from "react-native";
 import { Card, Text } from "@rneui/themed";
 import { Container } from "../../../../style";
 const Category = ({ content, navigation }) => (
-
   <View>
     <Container>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("ListCourse", { code: content.code })}
-    >
-      <Card>
-        <Card.Title>
-          <Text h4>{content.title}</Text>
-        </Card.Title>
-
-      </Card>
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("ListCourse", { code: content.code })
+        }
+      >
+        <Card>
+          <Card.Title>
+            <Text h4>{content.title} {content.order}</Text>
+          </Card.Title>
+        </Card>
+      </TouchableOpacity>
     </Container>
   </View>
 );
 
 export default Category;
-
--- src/features/course/hooks/useCategory.js
+++ src/features/course/hooks/useCategory.js
@@ -4,8 +4,10 @@ import courseClient from "../../../api/courseClient";
 const useCategory = () => {
   const [category, setCategory] = useState([]);
 
-  const sortCategory = ( categories ) => {
-    return categories;
+  const sortCategory = (categories) => {
+    return categories.sort((a, b) => {
+      return a.order > b.order;
+    });
   };
 
   const fetchApi = () => {
