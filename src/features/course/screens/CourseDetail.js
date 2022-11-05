7c8346d80c9f9181b61e1fb19285d00d7485761c
Karthick m <karthickmohan419@gmail.com>
2022-11-05 07:25:01 +0530

    hooks

--- src/features/course/screens/CourseDetail.js
+++ src/features/course/screens/CourseDetail.js
@@ -1,37 +1,57 @@
-import { React, useEffect } from "react";
-import { Text, Card, View } from "react-native";
-import { CourseImage } from "../../../../style";
+import { React, useEffect, useState } from "react";
+import { Text, Card, View, Share } from "react-native";
+import Course from "../../../components/Course";
+import courseClient from "../../../api/courseClient";
+import { CourseImage, Container } from "../../../../style";
 
-const CourseDetail = ({ course, route }) => {
-  // const fetchApi = () => {
-  //   courseClient
-  //     .get("/course", { params: { ctgry: route.params.code } })
-  //     .then((response) => {
-  //       setcourses(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error :", error);
-  //     });
-  // };
+const CourseDetail = ({ route, navigation }) => {
+  const [course, setcourses] = useState([]);
+
+  const renderCourseCard = ({ item }) => (
+    <Course course={item} navigation={navigation} />
+  );
+
+  const CourseDetail = () => {
+    courseClient
+      .get("/course", { params: { ctgry: route.params.code } })
+      .then((response) => {
+        setcourses(response.data);
+      })
+      .catch((error) => {
+        console.log("Error :", error);
+      });
+  };
 
   useEffect(() => {
-    // fetchApi();
+    CourseDetail();
   }, []);
 
   return (
-    <Card>
-      <CourseImage
-        resizeMode="stretch"
-        source={{
-          uri: "https://reactnative.dev/img/tiny_logo.png",
-        }}
-      />
-      <View>
-        <Text>{course.title}</Text>
-        <Text>{course.author}</Text>
-        <Text>{course.price}</Text>
-      </View>
-    </Card>
+    <>
+      <Container>
+        <FlatList
+          data={course}
+          renderItem={renderCourseCard}
+          keyExtractor={(item) => item.course_id}
+        />
+      </Container>
+      <Card>
+        <CourseImage
+          resizeMode="stretch"
+          source={{
+            uri: "https://reactnative.dev/img/tiny_logo.png",
+          }}
+        />
+        {
+          <View>
+            <View>{<Button onPress={Share} />}</View>
+            <Text>{course.title}</Text>
+            <Text>{course.author}</Text>
+            <Text>{course.price}</Text>
+          </View>
+        }
+      </Card>
+    </>
   );
 };
 
