a6abf44a115be98ec0522ccaf2932f70fda55a1c
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-11-11 21:51:40 -0500

    Course mode skeleton

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,6 +1,7 @@
 import { View, TouchableOpacity } from "react-native";
 import { Text, Card } from "@rneui/themed";
 import { CourseAuthor, CourseImage, CourseTitle, FlexWrap } from "../../style";
+import { Icon } from "@rneui/base";
 
 const Course = ({ course, navigation }) => (
   <View>
@@ -13,10 +14,27 @@ const Course = ({ course, navigation }) => (
             <CourseTitle>
               <Text>{course.title}</Text>
             </CourseTitle>
-            <CourseAuthor>
-              <Text>{course.author}</Text>
-            </CourseAuthor>
-            <Text>{course.price}</Text>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ flexDirection: "column", flex: 2 }}>
+                <CourseAuthor>
+                  <Text>{course.author}</Text>
+                </CourseAuthor>
+                <Text>{course.price}</Text>
+              </View>
+              <View style={{ flexDirection: "row", flex: 1 }}>
+                <Icon
+                  name="headphones"
+                  type="font-awesome-5"
+                  style={{ margin: 8 }}
+                />
+                <Icon
+                  name="google-classroom"
+                  type="material-community"
+                  style={{ margin: 8 }}
+                />
+                <Icon name="people" type="material" style={{ margin: 8 }} />
+              </View>
+            </View>
           </View>
         </Card>
       </TouchableOpacity>
