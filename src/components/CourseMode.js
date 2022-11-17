38279ab729b010427ed63ee4e604aa2c4ff409c0
Arjun <arjun.251106@gmail.com>
2022-11-17 12:05:56 +0530

    coursemode

--- src/components/CourseMode.js
+++ src/components/CourseMode.js
@@ -4,26 +4,34 @@ import { Icon } from "@rneui/base";
 
 const CourseMode = ({ course }) => {
   // FIXME - Determine the iconName & Family based on course mode using switch case
-  const iconName = ({ iconName }) => {
-    iconName = "headphones";
-    switch (iconName) {
-    case "headphones":
-      console.log("headphones");
-      break;
-
-      // const iconFamily = "font-awesome-5";
-      // switch (iconFamily) {
-      //   case "font - awesome - 5":
-      //     console.log("font-awesome-5")
-      // }
-    }
-  };
+  const iconName = "headphones";
+  const iconFamily = "font-awesome-5";
+  switch (course.mode) {
+  case "recorded":
+    console.log("headphones");
+    break;
+  case "online":
+    console.log("online");
+    break;
+  case "inperson":
+    console.log("inperson");
+    break;
+    //   case "recorded":
+    //     console.log("font-awesome-5");
+    //     break;
+    //   case "online":
+    //     console.log(" google-classroom material-community");
+    //     break;
+    //   case "inperson":
+    //     console.log("people material");
+    //     break;
+  }
 
   // headphones font-awesome-5 (recorded)
   // google-classroom material-community (online)
   // people material (inperson)
-  // console.log(" Course : ", course.mode);
-  return <Icon name={iconName} />;
+  //  console.log(" Course : ", course.mode);
+  return <Icon name={iconName} type={iconFamily} />;
 };
 
 export default CourseMode;
