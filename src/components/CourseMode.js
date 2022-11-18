2db6b5132e705aa56c7d6d5a28a9a9240c0d7d0e
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-11-17 22:34:01 -0500

    Refacrored

--- src/components/CourseMode.js
+++ src/components/CourseMode.js
@@ -3,29 +3,27 @@ import { Icon } from "@rneui/base";
 // style={{ margin: 8 }}   //FIXME - Get this from styled component
 
 const CourseMode = ({ course }) => {
-  // FIXME - Determine the iconName & Family based on course mode using switch case
-  const iconName = "headphones";
-  switch (course.mode) {
-  case "recorded":
-    console.log("headphones");
-    break;
-  case "online":
-    console.log("online");
-    break;
-  case "inperson":
-    console.log("inperson");
-    break;
-  }
-  const iconFamily = "font-awesome-5";
-  switch (course.mode) {
-  case "recorded":
-    console.log("font-awesome-5");
-    break;
-  }
-
   // headphones font-awesome-5 (recorded)
   // google-classroom material-community (online)
   // people material (inperson)
+
+  let iconName = null;
+  let iconFamily = null;
+
+  switch (course.mode) {
+  case "recorded":
+    iconName = "headphones";
+    iconFamily = "font-awesome-5";
+    break;
+  case "inperson":
+    iconName = "people";
+    iconFamily = "material";
+    break;
+  default:
+    iconName = "google-classroom";
+    iconFamily = "material-community";
+    break;
+  }
   console.log(" Course : ", course.mode);
   return <Icon name={iconName} type={iconFamily} />;
 };
