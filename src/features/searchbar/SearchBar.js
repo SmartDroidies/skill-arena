2918bbb78af7f9f9490faedb576769ea843fcd3e
lokesh <lokeshrajeshbabu28@gmail.com>
2022-11-30 08:25:04 +0530

    styling

--- src/features/searchbar/SearchBar.js
+++ src/features/searchbar/SearchBar.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_URL } from "@env";
+const SearchBar = ({ }) => {
+  const [searchTerm, setSearchTerm] = useState([]);
+  const [data, setData] = useState([]);
 
-const SearchBar = ({ searchTerm }) => {
+  const fetchApi = async () => {
+    try {
+      const response = await axios.get("https://xmp3cyul14.execute-api.ap-south-1.amazonaws.com/Prod")
+      console.log(data.results);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchApi();
+  }, [API_URL]);
+
   return (
     <View style={styles.searchBar}>
       <FontAwesome5 name="search" color="black" style={styles.iconSearch} />
-      <TextInput placeholder="Search" />
+      <TextInput
+        placeholder="Search"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} />
     </View>
   );
 };
@@ -14,6 +35,9 @@ const SearchBar = ({ searchTerm }) => {
 const styles = StyleSheet.create({
   searchBar: {
     flexDirection: "row",
+    marginTop: 15,
+  },
+  iconSearch: {
     marginTop: 15,
   },
 });
