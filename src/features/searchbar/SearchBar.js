4e7df1b4c0c036ea6f78f3512a9642de9a4f1def
lokesh <lokeshrajeshbabu28@gmail.com>
2022-12-01 08:10:39 +0530

    styling

--- src/features/searchbar/SearchBar.js
+++ src/features/searchbar/SearchBar.js
@@ -1,33 +1,30 @@
-import React from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import { TextInput, StyleSheet, View } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { API_URL } from "@env";
-const SearchBar = ({ }) => {
-  const [searchTerm, setSearchTerm] = useState([]);
-  const [data, setData] = useState([]);
+import courseClient from "../../api/courseClient";
 
-  const fetchApi = async () => {
-    try {
-      const response = await axios.get("https://xmp3cyul14.execute-api.ap-south-1.amazonaws.com/Prod")
-      console.log(data.results);
-      setData(response.data);
-    } catch (error) {
-      console.log(error);
-    }
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState([]);
+
+  const fetchApi = () => {
+    courseClient
+      .post("/search")
+      .then((response) => {
+        setSearchTerm(response.data);
+      })
+      .catch((error) => {
+        console.log("Error :", error);
+      });
   };
+
   useEffect(() => {
     fetchApi();
-  }, [API_URL]);
+  }, []);
 
   return (
     <View style={styles.searchBar}>
       <FontAwesome5 name="search" color="black" style={styles.iconSearch} />
-      <TextInput
-        placeholder="Search"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)} />
+      <TextInput placeholder="Search" value={searchTerm} />
     </View>
   );
 };
