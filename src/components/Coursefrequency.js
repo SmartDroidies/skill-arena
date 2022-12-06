3b5e96ad89716999f02826cb9ec5af62870cdcdb
Arjun <arjun.251106@gmail.com>
2022-12-06 11:40:15 +0530

    refactored frequency

--- src/components/Coursefrequency.js
+++ src/components/Coursefrequency.js
@@ -1,22 +1,20 @@
+import { Text } from "react-native";
+
 const CourseFrequency = ({ course }) => {
   console.log(course);
   let frequencyName = null;
-  let priceName = null;
   switch (course.frequency) {
   case "single":
     frequencyName = "Single Session";
-    priceName = "Rs 50 or Free";
     break;
   case "multi":
     frequencyName = "4 Sessions";
-    priceName = "Rs 200 or Free";
     break;
   default:
     frequencyName = "recurring";
-    priceName = "Rs 200 per Month";
     break;
   }
-  return <frequency name={frequencyName} price={priceName} />;
+  return <Text>{frequencyName}</Text>;
 };
 
 export default CourseFrequency;
