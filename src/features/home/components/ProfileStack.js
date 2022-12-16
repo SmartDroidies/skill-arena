1ec9e7b2138c5c60bfd430238a385352ad7530b2
karthick <karthickmohan419@gmail.com>
2022-12-16 08:05:00 +0530

    profile

---
+++ src/features/home/components/ProfileStack.js
@@ -0,0 +1,19 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import Global from "../../../utils/Global";
+import Signin from "../../profile/screens/signin";
+
+export const AppStack = createStackNavigator();
+
+export const AppNavigator = () => {
+  return (
+    <AppStack.Navigator screenoptions={{ headerShown: true }}>
+      <AppStack.Screen
+        name="Signin"
+        component={Signin}
+        options={{
+          title: Global.Constant.appName,
+        }}
+      />
+    </AppStack.Navigator>
+  );
+};
---
+++ src/features/profile/screens/Signin.js
@@ -0,0 +1,7 @@
+import { StyleSheet, View } from "react-native";
+
+const Signin = () => <View style={styles.sample}></View>;
+
+const styles = StyleSheet.create({});
+
+export default Signin;
