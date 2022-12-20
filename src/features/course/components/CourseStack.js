188d69197592453a2fc1469443ea6289436ef75d
lokesh <lokeshrajeshbabu28@gmail.com>
2022-12-20 22:29:48 +0530

    colour accross screens

--- src/features/course/components/CourseStack.js
+++ src/features/course/components/CourseStack.js
@@ -2,12 +2,22 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ListCategory from "../screens/ListCategory";
 import ListCourse from "../screens/ListCourse";
 import Global from "../../../utils/Global";
+import { useTheme } from "styled-components";
 
 export const CourseStack = createStackNavigator();
 
 export const CourseNavigator = () => {
+  const theme = useTheme();
   return (
-    <CourseStack.Navigator screenoptions={{ headerShown: true }}>
+    <CourseStack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: theme.PRIMARY_COLOR,
+        },
+        headerTintColor: theme.BACKGROUND_COLOR,
+      }}
+    >
       <CourseStack.Screen
         name="ListCategory"
         component={ListCategory}
