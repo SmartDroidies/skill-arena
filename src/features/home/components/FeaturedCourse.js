8e11f0612c69d005e0718115a3c2d038bff28056
lokesh <lokeshrajeshbabu28@gmail.com>
2022-12-28 19:36:15 +0530

    refactored

--- src/features/home/components/FeaturedCourse.js
+++ src/features/home/components/FeaturedCourse.js
@@ -1,14 +1,16 @@
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import { Card } from "@rneui/themed";
 import {
   CourseAuthor,
   CourseImage,
   CourseTitle,
   FlexWrap,
+  FlexView,
   FrequencyView,
-} from "../../../../style";
+  CourseView } from "../../../../style";
 import { courseImage } from "../../../utils/ImageUtil";
 import CourseFrequency from "../../../components/CourseFrequency";
+import CourseMode from "../../../components/CourseMode";
 
 const FeaturedCourse = ({ course, navigation }) => (
   <View>
@@ -28,13 +30,21 @@ const FeaturedCourse = ({ course, navigation }) => (
             <CourseTitle>
               <Text>Home screen : {course.title}</Text>
             </CourseTitle>
-            <CourseAuthor>
-              <Text>{course.author}</Text>
-            </CourseAuthor>
-            <Text>{course.price}</Text>
-            <FrequencyView>
-              <CourseFrequency course={course} />
-            </FrequencyView>
+
+            <FlexView direction="row">
+              <FlexView direction="column">
+                <CourseAuthor>
+                  <Text>{course.author}</Text>
+                </CourseAuthor>
+                <Text>{course.price}</Text>
+                <FrequencyView>
+                  <CourseFrequency course={course} />
+                </FrequencyView>
+              </FlexView>
+              <CourseView>
+                <CourseMode course={course} />
+              </CourseView>
+            </FlexView>
           </View>
         </Card>
       </TouchableOpacity>
