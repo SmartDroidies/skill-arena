e572e648bb49673194ca4ada72b59117e63448dd
Prasath Basuvaraj <prasath.buddie@gmail.com>
2022-12-28 22:40:06 -0500

    Fixing Url

--- src/api/courseClient.js
+++ src/api/courseClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_URL } from "@env";
 
 const courseClient = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL.trim(),
 });
 
 courseClient.interceptors.request.use((request) => {
